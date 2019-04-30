5f4fc2b31d6f35920ed10d834d63d15ad0537592
koskas <koskas@yahoo.com>
2019-04-30 03:14:23 +0300

    25 years passed since i wrote a single line of code in c language

---
+++ c/level-1/c-1.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("int: %d Bytes \n", sizeof(int));
+    printf("float: %d Bytes \n", sizeof(float));
+    printf("double: %d Bytes \n", sizeof(double));
+    printf("char: %d Bytes \n", sizeof(char));
+
+    return 0;
+}
